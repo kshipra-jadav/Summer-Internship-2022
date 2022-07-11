743e05d94f09a6b485f6c0ea039481a4e8785716
kshipra-jadav <jadavkshipra2@gmail.com>
2022-07-11 21:22:34 +0530

    Added Quiz 3

--- Week2/Day1/all.js
+++
@@ -1,8 +0,0 @@
-const set1 = new Set([1, 2])
-console.log(set1) // Output :- Set(2) { 1, 2 }
-
-const set2 = new Set(["Hello", "Everybody"])
-console.log(set2) // Output :- Set(2) { 'Hello', 'Everybody' }
-
-const set3 = new Set([...set1, ...set2])
-console.log(set3) // Output :- Set(4) { 1, 2, 'Hello', 'Everybody' }
